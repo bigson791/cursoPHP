6f47961f42f07e11b57ac374698a704296c5b777
Elfego Apen <bigson791@gmail.com>
2021-05-02 23:25:32 -0600

    actualizando el curso

--- 18-Formularios/Guardar.php
+++ 18-Formularios/Guardar.php
@@ -3,6 +3,6 @@
 if(isset($_POST['titulo']) && isset($_POST['descripcion'])){
     
     echo "<h1>".$_POST['titulo']."</h>";
-    echo "<h1>".$_POST['descripcion']."</h>";       
+    echo "<h2>".$_POST['descripcion']."</h2>";       
             
 }
