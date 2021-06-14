11898128e7b798709b7b6bd95aa7512107e4318d
Elfego <Elfego@Elfego-HP>
2021-06-14 13:54:09 -0600

    actualizando el archivo

--- 20-ficheros/index.php
+++ 20-ficheros/index.php
@@ -3,7 +3,7 @@
 // abrir un archivo... modo, r = lectura, w = escritura, x=ejecucion, a+ lectura y escritura
 $archivo = fopen("fichero_texto.txt", "a+");
 
-
+/*
 while (!feof($archivo)) {
     //leer archivo
     $contenido = fgets($archivo);
@@ -16,3 +16,12 @@ fwrite($archivo, "<br>texto agregado desde php");
 
 //Cerrar archivo
 fclose($archivo);
+*/
+//Copiar
+//copy('fichero_texto.txt','fichero_copiado.txt') or die("Error al copiar");
+
+//renombrar
+//rename('fichero_copiado.txt','nombre_cambiado.txt');
+
+//Eliminar
+unlink('nombre_cambiado.txt')or die("Error al borrar");
