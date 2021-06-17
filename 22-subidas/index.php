d1dedb29d1b9c53d3a6871f3a3654e56918f7b29
Elfego Apen <bigson791@gmail.com>
2021-06-16 23:45:14 -0600

    actualizando el contenido del curso.

--- 22-subidas/index.php
+++ 22-subidas/index.php
@@ -11,6 +11,17 @@
             <input type="file" name="archivo">
             <input type="submit" name="Enviar">
         </form>
-        
+        <h1>Listado de Imagenes</h1>
+
+        <?php
+        $gestor = opendir('./images');
+        if ($gestor):
+            while (($image = readdir($gestor)) !== false)://este tipo de sintaxis se usa cuando  va codigo php en una vista.
+                if ($image != '.' && $image != '..'):
+                    echo "<img src='images/$image' width='200px'/><br>";
+                endif;//este tipo de sintaxis se usa cuando  va codigo php en una vista.
+            endwhile;//este tipo de sintaxis se usa cuando  va codigo php en una vista.
+        endif;//este tipo de sintaxis se usa cuando  va codigo php en una vista.
+        ?>
     </body>
 </html>
